98826b5d584d425da7ca965b1dd6f7b0389c1728
arifinfrds <arifinfrds.engineer@gmail.com>
2022-12-10 19:58:12 +0700

    add logic for reverse a string

---
+++ ReverseAStringTests/ReverseAStringTests.swift
@@ -0,0 +1,62 @@
+//
+//  ReverseAStringTests.swift
+//  ReverseAStringTests
+//
+//  Created by arifinfrds.engineer on 10/12/22.
+//
+
+import XCTest
+
+// GIVEN has empty string
+// WHEN executed
+// THEN should return empty string
+
+// GIVEN has single string count
+// WHEN executed
+// THEN should return that string
+
+// GIVEN has string of "ab"
+// WHEN executed
+// THEN should return "ba"
+
+struct ReverseAString {
+    func reverse(string: String) -> String {
+        
+        if string.isEmpty {
+            return string
+        }
+        
+        if string.count == 1 {
+            return string
+        }
+        
+        return String(string.reversed())
+    }
+}
+
+final class ReverseAStringTests: XCTestCase {
+
+    func test_reverse_shouldNotReverseOnEmptyString() {
+        let sut = ReverseAString()
+        
+        let result = sut.reverse(string: "")
+        
+        XCTAssertTrue(result.isEmpty)
+    }
+    
+    func test_reverse_shouldNotReverseOnSingleStringCount() {
+        let sut = ReverseAString()
+        
+        let result = sut.reverse(string: "a")
+        
+        XCTAssertEqual(result, "a")
+    }
+    
+    func test_reverse_shouldConvertsOnMoreThanSingleStringCount() {
+        let sut = ReverseAString()
+        
+        let result = sut.reverse(string: "ab")
+        
+        XCTAssertEqual(result, "ba")
+    }
+}
